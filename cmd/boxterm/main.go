0bd04f52bbef101b504734e804e33917e9336818
Edgar Lee <edgarl@netflix.com>
2018-06-08 14:31:15 -0700

    Switch boxterm to use tcell

--- cmd/boxterm/main.go
+++ cmd/boxterm/main.go
@@ -8,91 +8,10 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/gdamore/tcell/termbox"
 	"github.com/hinshun/vt10x"
-	termbox "github.com/james4k/termbox-go-noinput"
 	"github.com/kr/pty"
 )
-
-func filterESC(r io.Reader) io.Reader {
-	pr, pw := io.Pipe()
-	go func() {
-		br := bufio.NewReader(r)
-		for {
-			b, err := br.ReadByte()
-			if err != nil {
-				pw.CloseWithError(err)
-				return
-			}
-			if b == 033 {
-				_, err = pw.Write([]byte("ESC"))
-			} else {
-				_, err = pw.Write([]byte{b})
-			}
-			if err != nil {
-				pw.CloseWithError(err)
-				return
-			}
-		}
-	}()
-	return pr
-}
-
-func update(term *vt10x.VT, state *vt10x.State, w, h int) {
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	for i := 0; i < h+2; i++ {
-		termbox.SetCell(0, i, '│', termbox.ColorDefault, termbox.ColorDefault)
-		termbox.SetCell(w+1, i, '│', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	for i := 0; i < w+2; i++ {
-		termbox.SetCell(i, 0, '─', termbox.ColorDefault, termbox.ColorDefault)
-		termbox.SetCell(i, h+1, '─', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	termbox.SetCell(0, 0, '┌', termbox.ColorDefault, termbox.ColorDefault)
-	termbox.SetCell(w+1, 0, '┐', termbox.ColorDefault, termbox.ColorDefault)
-	termbox.SetCell(w+1, h+1, '┘', termbox.ColorDefault, termbox.ColorDefault)
-	termbox.SetCell(0, h+1, '└', termbox.ColorDefault, termbox.ColorDefault)
-
-	state.Lock()
-	defer state.Unlock()
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			c, fg, bg := state.Cell(x, y)
-			/*
-				// termbox only supports 8 colors
-				if fg > 15 {
-					fg = 7
-				} else if fg > 7 {
-					fg -= 8
-				}
-				if bg > 15 {
-					bg = 0
-				} else if bg > 7 {
-					bg -= 8
-				}
-			*/
-			fg = 6 // colors are an issue for later; just keep it monocolored for now
-			bg = 0
-			termbox.SetCell(x+1, y+1, c,
-				termbox.Attribute(fg+1),
-				termbox.Attribute(bg+1))
-		}
-	}
-	if state.CursorVisible() {
-		curx, cury := state.Cursor()
-		curx += 1
-		cury += 1
-		termbox.SetCursor(curx, cury)
-	} else {
-		termbox.SetCursor(-1, -1)
-	}
-	termbox.Flush()
-}
-
-func logpanic() {
-	if x := recover(); x != nil {
-		fmt.Fprintln(os.Stderr, x)
-	}
-}
 
 func main() {
 	// NOTE: This must be before termbox.Init(). On OSX, at least, we get a
@@ -168,5 +87,62 @@ func main() {
 		case <-updatec:
 			update(term, &state, wide-2, tall-2)
 		}
+	}
+}
+
+func update(term *vt10x.VT, state *vt10x.State, w, h int) {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	for i := 0; i < h+2; i++ {
+		termbox.SetCell(0, i, '│', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(w+1, i, '│', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	for i := 0; i < w+2; i++ {
+		termbox.SetCell(i, 0, '─', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(i, h+1, '─', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	termbox.SetCell(0, 0, '┌', termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(w+1, 0, '┐', termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(w+1, h+1, '┘', termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(0, h+1, '└', termbox.ColorDefault, termbox.ColorDefault)
+
+	state.Lock()
+	defer state.Unlock()
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			c, fg, bg := state.Cell(x, y)
+			/*
+				// termbox only supports 8 colors
+				if fg > 15 {
+					fg = 7
+				} else if fg > 7 {
+					fg -= 8
+				}
+				if bg > 15 {
+					bg = 0
+				} else if bg > 7 {
+					bg -= 8
+				}
+			*/
+			fg = 6 // colors are an issue for later; just keep it monocolored for now
+			bg = 0
+			termbox.SetCell(x+1, y+1, c,
+				termbox.Attribute(fg+1),
+				termbox.Attribute(bg+1))
+		}
+	}
+	if state.CursorVisible() {
+		curx, cury := state.Cursor()
+		curx += 1
+		cury += 1
+		termbox.SetCursor(curx, cury)
+	} else {
+		termbox.SetCursor(-1, -1)
+	}
+	termbox.Flush()
+}
+
+func logpanic() {
+	if x := recover(); x != nil {
+		fmt.Fprintln(os.Stderr, x)
 	}
 }
