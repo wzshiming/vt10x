c8cd298b0471f699d16db3589da6254177bd0de5
Edgar Lee <edgarl@netflix.com>
2018-06-08 10:01:26 -0700

    Add test for DSR CPR

--- vt_test.go
+++ vt_test.go
@@ -1,9 +1,18 @@
 package vt10x
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
+
+	expect "github.com/Netflix/go-expect"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func extractStr(t *State, x0, x1, row int) string {
@@ -67,4 +76,78 @@ func TestNewline(t *testing.T) {
 	if fg != DefaultFG {
 		t.Fatal(st.cur.x, st.cur.y, fg, bg)
 	}
+}
+
+var (
+	dsrPattern = regexp.MustCompile(`(\d+);(\d+)`)
+)
+
+type Coord struct {
+	row int
+	col int
+}
+
+func TestVTCPR(t *testing.T) {
+	c, err := expect.NewConsole()
+	require.NoError(t, err)
+	defer c.Close()
+
+	var state State
+	term, err := Create(&state, c)
+	require.NoError(t, err)
+	defer term.Close()
+
+	go func() {
+		for {
+			err := term.Parse()
+			if err != nil {
+				t.Log(err)
+				break
+			}
+		}
+	}()
+
+	coord, err := cpr(c.Tty())
+	require.NoError(t, err)
+	require.Equal(t, 1, coord.row)
+	require.Equal(t, 1, coord.col)
+}
+
+// cpr is an example application that requests for the cursor position report.
+func cpr(tty *os.File) (*Coord, error) {
+	oldState, err := terminal.MakeRaw(int(tty.Fd()))
+	if err != nil {
+		return nil, err
+	}
+
+	defer terminal.Restore(int(tty.Fd()), oldState)
+
+	// ANSI escape sequence for DSR - Device Status Report
+	// https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_sequences
+	fmt.Fprint(tty, "\x1b[6n")
+
+	// Reports the cursor position (CPR) to the application as (as though typed at
+	// the keyboard) ESC[n;mR, where n is the row and m is the column.
+	reader := bufio.NewReader(tty)
+	text, err := reader.ReadSlice('R')
+	if err != nil {
+		return nil, err
+	}
+
+	matches := dsrPattern.FindStringSubmatch(string(text))
+	if len(matches) != 3 {
+		return nil, fmt.Errorf("incorrect number of matches: %d", len(matches))
+	}
+
+	col, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return nil, err
+	}
+
+	row, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Coord{row, col}, nil
 }
