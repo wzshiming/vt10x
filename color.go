9e60c576db0266b7b9123b7b94df87bb75d4495c
Shiming Zhang <wzshiming@hotmail.com>
2024-11-01 18:18:58 +0800

    Support rgb color

--- color.go
+++ color.go
@@ -34,5 +34,29 @@ type Color uint32
 
 // ANSI returns true if Color is within [0, 16).
 func (c Color) ANSI() bool {
-	return (c < 16)
+	return c < 16
+}
+
+func (c Color) RGB() (r, g, b uint8, ok bool) {
+	if c < 16 {
+		return 0, 0, 0, false
+	}
+	if c < 232 {
+		c -= 16
+		r = uint8(c / 36)
+		g = uint8((c % 36) / 6)
+		b = uint8(c % 6)
+		r = r * 51
+		g = g * 51
+		b = b * 51
+		return r, g, b, true
+	}
+	if c < 256 {
+		g = uint8((c-232)*10 + 8)
+		return g, g, g, true
+	}
+	if c < 1<<24 {
+		return uint8(c >> 16), uint8(c >> 8), uint8(c), true
+	}
+	return 0, 0, 0, false
 }
